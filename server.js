3934e7fcd2fef36dde236daa315907af029ddac0
Warren Uhrich <hello@warren.codes>
2024-05-14 13:36:05 -0600

    Add hello/:name route and page

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get('/hello/:name', (req, res) => res.end(`
+<!DOCTYPE html>
+<html>
+    <head><title>Hello, ${req.params.name}!</title></head>
+    <body><h1>Hello, ${req.params.name}!</h1></body>
+</html>
+`));
+
 app.listen(port, err => {
     console.log(err || `listening on port ${port} 😎`);
 });
